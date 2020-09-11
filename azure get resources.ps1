35a3a3fe7b67f3a43396733c3560e463ea308d7e
Kossisochukwu Okafor <kossytony@gmail.com>
2020-09-11 02:20:36 -0400

    Powershell script for azure task

---
+++ azure get resources.ps1
@@ -0,0 +1,18 @@
+﻿$subscriptions = Get-AzSubscription
+#$subscriptionsID = ($subscriptions).id
+
+foreach ($subscription in $subscriptions){
+$context = Set-AzContext $subscription.Id
+$label = $subscription.Name
+switch($subscription.TenantId){
+"TenantID1"{$Tenant = "tenameName1"}
+"TenantID2"{$Tenant = "tenameName2"}
+"TenantID2"{$Tenant = "tenameName3"}
+}
+#$Tenant = (Get-AzTenant -TenantId $subscription.TenantId).name
+$myobject= Get-AzResource -DefaultProfile $context | select-object Name,ResourceGroupName,Location,Type 
+$myobject | Add-Member -NotePropertyName subscriptionID -NotePropertyValue $subscription.Id
+$myobject | Add-Member -NotePropertyName TenantName -NotePropertyValue $Tenant
+$pathname = $label+"-"+$Tenant
+$myobject|Export-Csv -path C:\Users\kokafor\Desktop\resources\$pathname.csv
+} 
